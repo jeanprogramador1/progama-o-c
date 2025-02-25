2b74f16748ee2810cec4d00a0e27ad5c95a94b32
jeanprogramador1 <jean.carlos1726917@gmail.com>
2025-02-25 18:09:27 +0000

    git commit

--- N1_NC5_2.c
+++ N1_NC5_2.c
@@ -1,1 +1,14 @@
+ #include <stdio.h>
 
+    int main () {
+        int idade = 18;
+        float altura = 1.76;
+        char opcao = 'D';
+        char nome[20] = "Jean";
+        
+        printf(" A idade de %s é: %d\n", nome, idade);
+        printf("A altura é: %e\n", altura);
+        printf("A opção é: %c\n", opcao);
+
+        return 0;
+    }
