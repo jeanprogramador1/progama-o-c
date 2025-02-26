4e2b4ca68f5cb2e7cb87f22a17ec678e5b58cbb7
jeanprogramador1 <jean.carlos1726917@gmail.com>
2025-02-26 23:49:17 +0000

    git commit

--- N1_NC5_3.c
+++ N1_NC5_3.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+    int main () {
+         
+        float populacao = 1.41;
+        char pais[20] = "china";
+        int ano_de_existencia = 4000;
 
+        printf("O seu país é: %s\n", pais);
+        printf("Tem uma população total de: %.2f bilhões\n", populacao);
+        printf("E seu ano de existencia é: %d anos\n", ano_de_existencia);
+
+        return 0;
+    }
