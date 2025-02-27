01a6481a4fcea64a6d515394333ed2f36a530f91
jeanprogramador1 <jean.carlos1726917@gmail.com>
2025-02-27 20:04:03 +0000

    git commit

--- Cadastro_de_aluno_em_C.c
+++ Cadastro_de_aluno_em_C.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+    int main () {
+        int idade, matricula;
+        float altura;
+        char nome[50];
+
+        printf("Digite sua idade: \n");
+        scanf("%d", &idade)
+
+        printf("Digite sua altura: \n");
+        scanf("%f", &altura);
+
+        printf("Digite seu nome: \n");
+        fgets(nome, 50, stdin);
+
+        printf("Digite o numero de matricula: \n");
+        scanf("%d", &matricula);
+
+        printf("Nome do aluno: %s - Matricula: %d", nome, matricula);
+        printf("Idade: %d - Altura: %.2f", idade, altura);
+
+        return 0;
+    }
