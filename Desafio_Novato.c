72a8f1fb9e2005d30830ba5f92c7a42854ceb938
jeanprogramador1 <jean.carlos1726917@gmail.com>
2025-03-03 18:17:32 +0000

    git commit

--- Desafio_Novato.c
+++ Desafio_Novato.c
@@ -1,1 +1,50 @@
+#include <stdio.h>
+    
+    int main () {
 
+        // Dando nome as variaveis e os seus valores.
+        char estado = 'A';
+        char codigo[50] = "A01";
+        char nome[50] = "SaoPaulo";
+        int populacao = 11900000;
+        float area = 248.21;
+        float pib = 920.833;
+        int npt = 36;
+
+        printf("Ola! Boa tarde, aqui abaixo esta o cadastro da primeira carta!\n");
+        printf("Carta 1: \n");
+        printf("Estado: %c\n", estado);
+        printf("Codigo: %s\n", codigo);
+        printf("Nome da cidade: %s\n", nome);
+        printf("População: %d\n", populacao);
+        printf("Area em Km²: %.2f Km²\n", area);
+        printf("PIB: %.3f reais\n", pib);
+        printf("Numero de Pontos Turisticos: %d\n", npt);
+
+        //Agora, vou utilizar as mesmas variaveis, entretanto, vou mudar os nomes significativos e os resultados.
+
+        char Estado = 'B';
+        char Codigo[50] = "B02";
+        char Nome[50] = "BeloHorizonte";
+        int Populacao = 21320000;
+        float Area = 586.51;
+        float PIB = 333.1;
+        int NPT = 5000;
+        //Aqui, nos nomes significativos, coloquei todos com no maximo 1 letra maiuscula, para ter uma diferença na variação no topo do codigo.
+
+        //Refazendo o Printf novamente.
+
+        printf("Agora, o cadastro da segunda carta!\n");
+        printf("Carta 2: \n");
+        printf("Estado: %c\n", Estado);
+        printf("Codigo: %s\n", Codigo);
+        printf("Nome da Cidade: %s\n", Nome);
+        printf("População: %d\n", Populacao);
+        printf("Area em Km²: %.2f Km²\n", Area);
+        printf("PIB: %.1f reais\n", PIB);
+        printf("Numero de Pontos Turisticos: %d\n", NPT);
+
+        printf ("Cadastro de duas cartas concluido, pode fechar o terminal!");
+
+        return 0;
+    }
