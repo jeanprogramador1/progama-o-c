0a5fa04bbf574e792b7804e823f430af8e3c0f0e
jeanprogramador1 <jean.carlos1726917@gmail.com>
2025-03-27 19:07:56 +0000

    git commit

--- Nivel_Aventureiro_Codar.C
+++ Nivel_Aventureiro_Codar.C
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+    int main () {
+        float nota1, nota2, nota3;
+        float media;
+        
+        printf("*** Programa de Cálculo de Média ***\n");
+        
+        printf("Digite a primeira nota do aluno: "); 
+        scanf(" %f", &nota1);
+
+        printf("Digite a segunda nota do aluno: ");
+        scanf(" %f", &nota2);
+
+        printf("Digita a terceira nota do aluno: ");
+        scanf(" %f", &nota3);
+
+        media = (nota1 + nota2 + nota3) / 3;
+
+        printf("A média dos valores é: %.2f", media);
+        return 0;
+    }
