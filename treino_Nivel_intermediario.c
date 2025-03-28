c4af9c12a49eff55fa94c5351a79a9d27caa808f
jeanprogramador1 <jean.carlos1726917@gmail.com>
2025-03-28 19:12:34 +0000

    git commit

--- treino_Nivel_intermediario.c
+++ treino_Nivel_intermediario.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+    
+   int main () {
+        int numeroNORMAL = 2147483647; //Valor maximo do int
+        long  int numerogrande = 2147483647;
+
+        printf("Numero normal: %d\n", numeroNORMAL);
+        printf("Numero grande: %ld\n", numerogrande);
+
+        numerogrande = 2147483648;
+
+        printf("Numero grande: %ld\n", numerogrande);
+
+        return 0;
+   }
 
