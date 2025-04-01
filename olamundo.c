64fc9cc6d0bc12e41ea3bf8223f481964fbd406e
jeanprogramador1 <jean.carlos1726917@gmail.com>
2025-04-01 16:24:30 +0000

    git commit

--- olamundo.c
+++ olamundo.c
@@ -1,8 +1,11 @@
- #include <stdio.h>
+#include <stdio.h>
 
     int main () {
 
-        printf("Ola, mundo!\n");
-
+        printf("Tamanho do int: %u bytes\n", sizeof(int));
+        printf("Tamanho do long int: %u bytes\n", sizeof(long int));
+        printf("Tamanho do long long int: %u bytes\n", sizeof(long long int));
+        printf("Tamanho do double: %u bytes\n", sizeof(double));
+        printf("Tamanho do long double: %u bytes\n", sizeof(long double));
         return 0;
     }
