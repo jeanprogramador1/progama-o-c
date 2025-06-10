915515f303dc488c93ab3a7c467fc3fc6dc63b41
jeanprogramador1 <jean.carlos1726917@gmail.com>
2025-06-10 12:40:23 +0000

    git commit

---
+++ treino_c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+    void menu() {
+        int opcao, numero1, numero2;
+        float resultado;
+
+        printf("\nOla! Bem vindo ao programa calculadora\n");
+        printf("Selecione uma das opções abaixo\n");
+        printf("1) Soma");
+        printf("2) Subtração");
+        printf("3) Divisão");
+        printf("4) Multiplicação");
+        scanf("%d", )
+
+    }
