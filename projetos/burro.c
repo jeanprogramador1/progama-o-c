5afba4e7c1f6f5b8cabb252099db0571da8b65ab
jeanprogramador1 <jean.carlos1726917@gmail.com>
2025-09-04 12:07:40 +0000

    git commit

---
+++ projetos/burro.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+    int main(){
+        int burro
+
+            printf("Voce é burro nivel: %d"burro);
+                if (burro <= 100000){
+                    burro++;
+                }
+                return 0;
+            }    
