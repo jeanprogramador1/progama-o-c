895f1f9855756b7b892b8c490b9b19e9ac345bca
jeanprogramador1 <jean.carlos1726917@gmail.com>
2025-09-10 12:08:38 +0000

    git commit

---
+++ meus_treinos/batalha_naval.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+    int main(){
+        char *linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+        int matriz[10],[10];
+            for(int i = 0; i < 10; i++){
+                
+            }
