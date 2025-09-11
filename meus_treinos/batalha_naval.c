7f0926b8b39598a66080135e859ba5d091dc7e71
jeanprogramador1 <jean.carlos1726917@gmail.com>
2025-09-11 17:38:19 +0000

    git commit

--- meus_treinos/batalha_naval.c
+++ meus_treinos/batalha_naval.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
     int main(){
-        char *linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-        int matriz[10],[10];
+        char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+        int coluna[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int matriz[10][10];
+
+            printf("----TABELA BATALHA NAVAL----\n");
+
+            
             for(int i = 0; i < 10; i++){
-                
+                printf("%c ", linha[i]);
             }
+
+            for(int j = 0; j < 10; j++){
+
+                printf("\n%d ", coluna[j]);
+            }
+            
+        }
---
+++ meus_treinos/treino_array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+    int main(){
+
+        char *nome[3] = {"IO", "X", "KOBU"};
+
+            for(int i = 0; i <= 3; i++){
+                printf("%s \n", nome[i]);
+            }
+        return 0;
+    }
